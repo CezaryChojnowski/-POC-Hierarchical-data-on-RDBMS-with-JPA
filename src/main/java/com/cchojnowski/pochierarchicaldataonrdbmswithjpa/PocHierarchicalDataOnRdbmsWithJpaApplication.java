42230a1bdf69d7cfac823bc59ddaff74ccde5637
CezaryChojnowski <chojnowski.czarek@gmail.com>
2023-02-11 16:20:04 +0100

    init commit

---
+++ src/main/java/com/cchojnowski/pochierarchicaldataonrdbmswithjpa/PocHierarchicalDataOnRdbmsWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cchojnowski.pochierarchicaldataonrdbmswithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHierarchicalDataOnRdbmsWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocHierarchicalDataOnRdbmsWithJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cchojnowski/pochierarchicaldataonrdbmswithjpa/PocHierarchicalDataOnRdbmsWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cchojnowski.pochierarchicaldataonrdbmswithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocHierarchicalDataOnRdbmsWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
